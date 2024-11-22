0b49df021acdf4b48b2abc83cc5c1436a48557dd
Emilyopg <emily780silva@gmail.com>
2024-11-22 16:43:27 -0300

    Primeira ferramenta de estudo

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'Programação',
+    'O que é csharp ?',
+    'O csharp é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'Qual a diferença entre uma lista e uma tupla em Python ?',
+    'Lista são mutáveis, tuplas são imutáveis'
+)
+
+criaCartao(
+    'Português',
+    ' O que é um sujeito na oração ?',
+    ' É o termo que indica quem pratica ou sofre a ação.'
+)
+
+criaCartao(
+    'Português',
+    ' O que é uma metáfora ?',
+    'É uma figura de linguagem que compara dois elementos diferentes, sem usar "como".'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é um relevo ?',
+    'É a forma da superfície terrestre, como montanhas, planícies e vales.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que são paralelos ?',
+    'Linhas horizontais no globo que indicam a latitude.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é fotossíntese ?',
+    'É o processo em que plantas transformam luz em energia química.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que são células ?',
+    'São as unidades básicas da vida que formam todos os seres vivos.'
+)
+
+criaCartao(
+    'Física',
+    'O que é a lei da gravitação universal ?',
+    'É a lei que afirma que todos os corpos se atraem com uma força proporcional às suas massas e inversamente proporcional ao quadrado da distância entre eles.'
+)
+
+criaCartao(
+    'Física',
+    'O que é velocidade ?',
+    'É a razão entre a distância percorrida e o tempo gasto para percorrê-la.'
+)
+
+criaCartao(
+    'Quimica',
+    'O que é uma reação química ?',
+    ' É o processo em que substâncias se transformam em novas substâncias, com alteração nas ligações químicas.'
+)
+
+criaCartao(
+    'Quimica',
+    'O que é pH ?',
+    'É uma medida da acidez ou alcalinidade de uma substância, variando de 0 a 14.'
+)
